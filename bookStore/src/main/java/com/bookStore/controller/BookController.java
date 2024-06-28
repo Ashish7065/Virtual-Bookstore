a55337341a334d1383b9609dfc18c2f71a69a1c5
Ashish7065 <118918858+Ashish7065@users.noreply.github.com>
2024-06-28 17:31:40 +0530

    files added

---
+++ bookStore/src/main/java/com/bookStore/controller/BookController.java
@@ -0,0 +1,76 @@
+package com.bookStore.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.bookStore.entity.Book;
+import com.bookStore.entity.MyBookList;
+import com.bookStore.service.BookService;
+import com.bookStore.service.MyBookListService;
+
+import java.util.*;
+
+@Controller
+public class BookController {
+	
+	@Autowired
+	private BookService service;
+	
+	@Autowired
+	private MyBookListService myBookService;
+	
+	@GetMapping("/")
+	public String home() {
+		return "home";
+	}
+	
+	@GetMapping("/book_register")
+	public String bookRegister() {
+		return "bookRegister";
+	}
+	
+	@GetMapping("/available_books")             //ModelView use for display the data in view
+	public ModelAndView getAllBook() {            //Use for get all list..
+		List<Book>list = service.getAllBook();  //this is use to display the all list of data in view..
+//		ModelAndView m=new ModelAndView();
+//		m.setViewName("bookList");
+//		m.addObject("book",list);
+		return new ModelAndView("bookList","book",list);  //Show in list to display...
+	}
+	
+	@PostMapping("/save")
+	public String addBook(@ModelAttribute Book b) {   // Use for save the data..
+		service.save(b);
+		return "redirect:/available_books";
+	}
+	@GetMapping("/my_books")
+	public String getMyBooks(Model model)            //Use for get the data
+	{
+		List<MyBookList>list=myBookService.getAllMyBooks();
+		model.addAttribute("book",list);
+		return "myBooks";
+	}
+	@RequestMapping("/mylist/{id}")
+	public String getMyList(@PathVariable("id") int id) {     //Use for get the particular data
+		Book b = service.getBookById(id);
+		MyBookList mb=new MyBookList(b.getId(),b.getName(),b.getAuthor(),b.getPrice());
+		myBookService.saveMyBooks(mb);
+		return "redirect:/my_books";
+	}
+	
+	@RequestMapping("/editBook/{id}")
+	public String editBook(@PathVariable("id") int id,Model model) {   //Use for update the particular data
+		Book b = service.getBookById(id);
+		model.addAttribute("book",b);
+		return "bookEdit";
+	}
+	@RequestMapping("/deleteBook/{id}")
+	public String deleteBook(@PathVariable("id")int id) {     //Use for delete the id..
+		service.deleteById(id);
+		return "redirect:/available_books";
+	}
+	
+}
